ec9435d7762273de5f94b593184baa796378c2b6
NarpathKumar <53571476+NarpathKumar@users.noreply.github.com>
2020-06-01 17:23:36 +0530

    Add files via upload

---
+++ Nuuk.js
@@ -0,0 +1,47 @@
+console.clear();
+var popup = document.getElementsByClassName("pop-up-list")[0];
+var overlay = document.getElementsByClassName("overlay-pop-up")[0];
+var getButton = document.getElementsByClassName("info-wrapper")[0];
+var button = document.getElementsByClassName("button")[0];
+var orangeButton = document.getElementById('orange-button');
+var closeBut = document.getElementsByClassName('pop-up-button')[0];
+var pcb = document.getElementsByClassName('close-button')[0];
+var line1 = document.getElementsByClassName('line-1')[0];
+var line2 = document.getElementsByClassName('line-2')[0];
+var phoneDrop = document.getElementsByClassName('phone-drop')[0];
+getButton.onclick=function(){
+	popup.style.display = "block";
+	overlay.style.display = "block";
+}
+button.onclick=function(){
+	popup.style.display = "block";
+	overlay.style.display = "block";
+	
+}
+orangeButton.onclick=function(){
+	popup.style.display = "block";
+	overlay.style.display = "block";
+}
+overlay.onclick=function(){
+	popup.style.display = "none";
+	overlay.style.display = "none";	
+}
+closeBut.onclick=function(){
+	popup.style.display = "none";
+	overlay.style.display = "none";	
+}
+pcb.onclick=function(){
+	var margin = window.getComputedStyle(line2).marginTop;
+	if(margin == "10px"){
+		phoneDrop.style.display = "flex";
+		line2.style.marginTop = "0px"
+		line1.style.transform = "rotateZ(54deg)";
+		line2.style.transform = "rotateZ(-54deg)";
+	}
+	if(margin == "0px"){
+		phoneDrop.style.display = "none";
+		line2.style.marginTop = "10px"
+		line1.style.transform = "rotateZ(0deg)";
+		line2.style.transform = "rotateZ(0deg)";
+	}
+}
